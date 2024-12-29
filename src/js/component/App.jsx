3185b425eea934ba28c21304e8997ecc0d3a7aea
Ricard0-A <ricardoarias4@hotmail.com>
2024-12-29 01:35:46 +0000

    got it

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   const [hoveredIndex, setHoveredIndex] = useState(null); 
 
+  
   const flexy = {
     display: 'flex',
     justifyContent: 'space-between',
@@ -13,6 +14,8 @@ const App = () => {
   };
 
   const handleDelete = (index) => {
+    // _, i Quiere decir que no quiero usar el unit, index del metodo Filter 
+    // Solo quiero usar el index, o abreviado ( I ) 
     setTodos(todos.filter((_, i) => i !== index));
   };
 
@@ -23,13 +26,18 @@ const App = () => {
         <ul>
           <li>
             <input
+              // Valor actual del input (Posteriormenete cambiara dinamicamente) que viene del useState
               value={inputValue}
               type="text"
               placeholder="What needs to be done?"
               onChange={(ev) => {
+                // Actualice el valor de inputValue y esto tambien afecta al valor actual del input
+                // ev es el evento origen de onChange, osea la accion de escribir luego .target luego .value 
+                // Se entiende
                 setInputValue(ev.target.value);
               }}
               onKeyUp={(ev) => {
+                // Si el evento origen.key (Lo que presiono) es === True...
                 if (ev.key === "Enter") {
                   if (inputValue.trim()) {
                     setTodos(todos.concat(inputValue));
@@ -39,7 +47,8 @@ const App = () => {
               }}
             />
           </li>
-
+          {/* metodo Map para iterar el array de todos que se ha ido llenando con los useState y los event listeners */}
+          {/* Poner el key = al index es obligatorio para que EL DOM identifique cada Li */}
           {todos.map((t, index) => (
             <li 
               key={index} 
@@ -48,8 +57,8 @@ const App = () => {
               onMouseLeave={() => setHoveredIndex(null)} 
             >
               {t}
-              <i 
-                onClick={() => handleDelete(index)} 
+              <i            
+                onClick={() => handleDelete(index)} // Que tarea va a borrar el handle Delete? La del index actual al momento de iterar (index de la tarea actual) 
                 className="fa-solid fa-x"
                 style={{ opacity: hoveredIndex === index ? 1 : 0 }}
               ></i>
@@ -58,6 +67,10 @@ const App = () => {
           <span>{todos.length} Item left</span>
         </ul>
         
+      <div>
+
+      </div>
+      
       </div>
       
     </div>
